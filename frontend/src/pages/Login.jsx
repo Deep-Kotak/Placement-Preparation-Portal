fc7b0c0ed5facac124fc594af3c5d5a281d4938b
Deep-Kotak <deepmkotak1103@gmail.com>
2026-06-01 22:48:51 +0530

    Added React frontend setup and login API integration

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import API from "../services/api";
+
+function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+
+    try {
+
+      const res = await API.post("/auth/login", {
+        email,
+        password
+      });
+
+      localStorage.setItem(
+        "token",
+        res.data.token
+      );
+
+      alert("Login Successful");
+
+      console.log(res.data);
+
+    } catch (error) {
+
+      alert(
+        error.response?.data?.message ||
+        "Login Failed"
+      );
+
+    }
+
+  };
+
+  return (
+    <div className="container mt-5">
+      <div
+        className="card shadow p-4 mx-auto"
+        style={{ maxWidth: "400px" }}
+      >
+        <h2 className="text-center mb-4">
+          Placement Portal Login
+        </h2>
+
+        <input
+          type="email"
+          className="form-control mb-3"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) =>
+            setEmail(e.target.value)
+          }
+        />
+
+        <input
+          type="password"
+          className="form-control mb-3"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) =>
+            setPassword(e.target.value)
+          }
+        />
+
+        <button
+          className="btn btn-primary w-100"
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
